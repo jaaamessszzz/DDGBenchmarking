8b57c3d7dbd60b3e188f2cab682aac8c38289f5c
oconchus <oconchus@fc5780ad-6544-46e6-b60b-86c75d3988a5>
2012-02-03 00:39:44 +0000

    Added a simple help file and classed up some examples, har har!
    git-svn-id: file:///kortemmelab/shared/commonSVN/ddg/trunk@576 fc5780ad-6544-46e6-b60b-86c75d3988a5

--- filterdemo.py
+++ filterdemo.py
@@ -1,21 +1,60 @@
 import common.ddgproject
+from ddglib.help import show_tables
 from ddglib.ddgfilters import *
 
-def printOutput(resultset):
-	print("Applying filters")
-	results = resultset.getFilteredResults()
-	print("After application")
-	print("\nSummary: %s\n" % resultset)
-	for r in results:
-		print(r["PDB_ID"], pickle.loads(r["BFactors"])["Total"])
-
-# Open a database connection
-ddGdb = common.ddgproject.ddGDatabase()
 
 # Create a result set
-sr = StructureResultSet(ddGdb, AdditionalIDs = ['2BQC', '1LAW', '1LHH', '1LHI'])
-sr = StructureResultSet(ddGdb)
-sr.addFilter(StructureFilter.TotalBFactors(0,16) | StructureFilter.WithNullResolution(True))
+class Examples:
+	
+	@staticmethod
+	def printOutput(resultset):
+		print("Applying filters")
+		results = resultset.getFilteredResults()
+		print("After application")
+		print("\nSummary: %s\n" % resultset)
 
-printOutput(sr)
+	@staticmethod
+	def openDB():
+		if not globals().get("ddGdb"):
+			globals()["ddGdb"] = common.ddgproject.ddGDatabase()
 
+	@staticmethod
+	def allStructures():
+		'''Select all Structure records.'''
+		print("** All strucures **") 
+		Examples.openDB()
+		sr = StructureResultSet(ddGdb)
+		Examples.printOutput(sr)
+
+	@staticmethod
+	def pickSpecific():
+		'''Select four specific Structure records and apply a filter.''' 
+		print("** 4 specific structures **") 
+		Examples.openDB()
+		sr = StructureResultSet(ddGdb, AdditionalIDs = ['2BQC', '1LAW', '1LHH', '1LHI'])
+		sr.addFilter(StructureFilter.TotalBFactors(0,16) | StructureFilter.WithNullResolution(True))
+		Examples.printOutput(sr)
+
+show_tables()
+if False:
+	# Step 1: Open a database connection
+	ddGdb = common.ddgproject.ddGDatabase()
+
+	# Step 2: Select database records
+	sr = StructureResultSet(ddGdb, AdditionalIDs = ['2BQC', '1LAW', '1LHH', '1LHI'])
+	
+	# Step 3: Add filters
+	sr.addFilter(StructureFilter.TotalBFactors(0,16) | StructureFilter.WithNullResolution(True))
+	
+	# Step 4: Retrieve full database records. 
+	# results will be a list each of whose elements is a dict representing a database record.
+	results = sr.getFilteredResults()
+	
+	# Step 5: Optionally print summary
+	print("\nSummary: %s\n" % sr)
+import sys
+sys.exit(0)
+
+#ddGdb = common.ddgproject.ddGDatabase()
+Examples.allStructures()
+Examples.pickSpecific()
