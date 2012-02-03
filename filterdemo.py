0b630cff7f18e224e33df77f57fb8b9d47578ad9
oconchus <oconchus@fc5780ad-6544-46e6-b60b-86c75d3988a5>
2012-02-03 23:30:36 +0000

    Beefed up the help file. We now print out available Filters (and their attributes and methods) and ResultSets automatically so long as they follow some conventions i.e. Filter (resp. ResultSet) in their name, private members prefixed with '_' etc.
    Added a lot more examples to filterdemo.py
    
    kobject: Added function for returning class functions, methods, and other attributes.
    Fixed a bug with escaping the wildcard % in a LIKE statement when there are existing string formatting.
    Fixed a bug where self.paramsoffset was not being reset before each application and so broke the assertion in Filter.
    Turn off some printing in ResultSet which should only be turned on for debugging.
    
    
    git-svn-id: file:///kortemmelab/shared/commonSVN/ddg/trunk@577 fc5780ad-6544-46e6-b60b-86c75d3988a5

--- filterdemo.py
+++ filterdemo.py
@@ -1,6 +1,25 @@
 import common.ddgproject
-from ddglib.help import show_tables
+from ddglib import help
 from ddglib.ddgfilters import *
+
+help.help()
+
+def simpleRunExample(self):
+	# Step 1: Open a database connection
+	ddGdb = common.ddgproject.ddGDatabase()
+
+	# Step 2: Select database records
+	sr = StructureResultSet(ddGdb, AdditionalIDs = ['2BQC', '1LAW', '1LHH', '1LHI'])
+	
+	# Step 3: Add filters
+	sr.addFilter(StructureFilter.TotalBFactors(0,16) | StructureFilter.WithNullResolution(True))
+	
+	# Step 4: Retrieve full database records. 
+	# results will be a list each of whose elements is a dict representing a database record.
+	results = sr.getFilteredResults()
+	
+	# Step 5: Optionally print summary
+	print("\nSummary: %s\n" % sr)
 
 
 # Create a result set
@@ -19,11 +38,55 @@ class Examples:
 			globals()["ddGdb"] = common.ddgproject.ddGDatabase()
 
 	@staticmethod
+	def help():
+		pass
+		#help.ShowDatabaseStructure()
+		#help.ShowResultSet()
+		#help.ShowFilter()
+
+	# UnionFilter examples
+
+	@staticmethod
+	def unionFilterExample1():
+		print("** All structures with null OR non-null resolution **") 
+		Examples.openDB()
+		sr = StructureResultSet(ddGdb)
+		sr.addFilter(StructureFilter.WithNullResolution(False) | StructureFilter.WithNullResolution(True))
+		Examples.printOutput(sr)
+
+	@staticmethod
+	def unionFilterExample2():
+		print("** All structures with null AND non-null resolution**") 
+		Examples.openDB()
+		sr = StructureResultSet(ddGdb)
+		sr.addFilter(StructureFilter.WithNullResolution(False))
+		sr.addFilter(StructureFilter.WithNullResolution(True))
+		Examples.printOutput(sr)
+
+
+	# StructureResultSet examples
+
+	@staticmethod
 	def allStructures():
 		'''Select all Structure records.'''
 		print("** All strucures **") 
 		Examples.openDB()
 		sr = StructureResultSet(ddGdb)
+		Examples.printOutput(sr)
+
+	@staticmethod
+	def getStructuresWithNullResolutionSQL():
+		print("** All structures with null resolution **") 
+		Examples.openDB()
+		sr = StructureResultSet(ddGdb, SQL = "WHERE Resolution IS NULL")
+		Examples.printOutput(sr)
+
+	@staticmethod
+	def getStructuresWithNullResolutionFilter():
+		print("** All structures with null resolution **") 
+		Examples.openDB()
+		sr = StructureResultSet(ddGdb)
+		sr.addFilter(StructureFilter.WithNullResolution(True))
 		Examples.printOutput(sr)
 
 	@staticmethod
@@ -35,26 +98,75 @@ class Examples:
 		sr.addFilter(StructureFilter.TotalBFactors(0,16) | StructureFilter.WithNullResolution(True))
 		Examples.printOutput(sr)
 
-show_tables()
-if False:
-	# Step 1: Open a database connection
-	ddGdb = common.ddgproject.ddGDatabase()
+	@staticmethod
+	def getStructuresInResolutionRange():
+		print("** All structures with null resolution **") 
+		Examples.openDB()
+		sr = StructureResultSet(ddGdb)
+		sr.addFilter(StructureFilter.Resolution(1, 2))
+		Examples.printOutput(sr)
+		
 
-	# Step 2: Select database records
-	sr = StructureResultSet(ddGdb, AdditionalIDs = ['2BQC', '1LAW', '1LHH', '1LHI'])
-	
-	# Step 3: Add filters
-	sr.addFilter(StructureFilter.TotalBFactors(0,16) | StructureFilter.WithNullResolution(True))
-	
-	# Step 4: Retrieve full database records. 
-	# results will be a list each of whose elements is a dict representing a database record.
-	results = sr.getFilteredResults()
-	
-	# Step 5: Optionally print summary
-	print("\nSummary: %s\n" % sr)
-import sys
-sys.exit(0)
+	# PredictionResultSet examples
 
+	@staticmethod
+	def getPredictionsWithSQL():
+		'''Select all Structure records.'''
+		print("** All structures **") 
+		Examples.openDB()
+		pr = PredictionResultSet(ddGdb, SQL = "WHERE PredictionSet=%s AND ID=12595", parameters = "testrun")
+		Examples.printOutput(pr)
+
+	@staticmethod
+	def getPredictionsUsingMultipleFilters():
+		'''This demonstrates the use of multiple filters.'''
+		print("** Multiple filter example **") 
+		Examples.openDB()
+		pr = PredictionResultSet(ddGdb, SQL = "WHERE PredictionSet=%s", parameters = "testrun")
+		pr.addFilter(StructureFilter.Techniques(StructureFilter.XRay))
+		#Examples.printOutput(pr)
+		pr.addFilter(StructureFilter.Resolution(1, 1.5) | StructureFilter.Resolution(3.9, 4))
+		#Examples.printOutput(pr)
+		pr.addFilter(StructureFilter.TotalBFactors(0, 10))
+		Examples.printOutput(pr)
+
+	@staticmethod
+	def getPredictionsUsingMultipleFilters_Speed():
+		'''This demonstrates how slow separate filters are.'''
+		print("** Multiple filter example **") 
+		Examples.openDB()
+
+		import time
+		
+		t1 = time.time()
+		pr = PredictionResultSet(ddGdb, SQL = "WHERE PredictionSet=%s", parameters = "testrun")
+		pr.addFilter(StructureFilter.Techniques(StructureFilter.XRay))
+		pr.addFilter(StructureFilter.Resolution(1, 1.5) | StructureFilter.Resolution(3.9, 4))
+		pr.addFilter(StructureFilter.TotalBFactors(0, 10))
+		Examples.printOutput(pr)
+		t2 = time.time()
+
+		print("Time taken: %0.2fs" % (t2 - t1))
+
+		t1 = time.time()
+		pr = PredictionResultSet(ddGdb, SQL = "WHERE PredictionSet=%s", parameters = "testrun")
+		sf = StructureFilter()
+		sf.setTechniques(StructureFilter.XRay)
+		sf.setResolution(1, 1.5)
+		sf.setTotalBFactors(0, 10)
+		pr.addFilter(sf | StructureFilter.Resolution(3.9, 4))
+		Examples.printOutput(pr)
+		t2 = time.time()
+		print("Time taken: %0.2fs" % (t2 - t1))
+
+	@staticmethod
+	def getPredictionsUsingMultipleFilters2():
+		print("** Multiple filter example **") 
+		Examples.openDB()
+		
 #ddGdb = common.ddgproject.ddGDatabase()
-Examples.allStructures()
-Examples.pickSpecific()
+Examples.help()
+Examples.getPredictionsUsingMultipleFilters_Speed()
+help.ShowFilter()
+#Examples.allStructures()
+#Examples.pickSpecific()
