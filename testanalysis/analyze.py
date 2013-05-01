66b9e930360cb5dfc2984a562e9eb7d398f2e1f5
oconchus <oconchus@fc5780ad-6544-46e6-b60b-86c75d3988a5>
2013-05-01 18:44:16 +0000

    Update to DDG run analysis script.
    git-svn-id: file:///kortemmelab/shared/commonSVN/ddg/trunk@1158 fc5780ad-6544-46e6-b60b-86c75d3988a5

--- testanalysis/analyze.py
+++ testanalysis/analyze.py
@@ -5,12 +5,52 @@ sys.path.insert(0, "../ddglib")
 from common import colortext, rosettadb
 import analysis
 
-analyzer = analysis.Analyzer("AllExperimentsProtocol16")
-analyzer.AddPublishedDDGsToAnalysisTables()
-reporter = analysis.Reporter(analyzer)
-reporter.CreateReport(description = analyzer.description)
+filetype = "pdf"
 
+import ddgdbapi
+ddGdb = ddgdbapi.ddGDatabase()
+results = ddGdb.execute("SELECT ID, ExperimentID, Status FROM Prediction WHERE PredictionSet=%s", parameters=('AllExperimentsProtocol16',))
 
+single_passed = 0
+single_failed = 0
+multiple_passed = 0
+multiple_failed = 0
+for r in results:
+	nummutations = len(ddGdb.execute("SELECT * FROM ExperimentMutation WHERE ExperimentID=%s", parameters=(r["ExperimentID"],)))
+	if nummutations > 1:
+		if r["Status"] == 'done':
+			multiple_passed += 1
+		elif r["Status"] == 'failed':
+			multiple_failed += 1
+		else:
+			raise Exception("Sd")
+	elif nummutations == 1:
+		if r["Status"] == 'done':
+			single_passed += 1
+		elif r["Status"] == 'failed':
+			single_failed += 1
+		else:
+			raise Exception("Sd")
+	else:
+		raise Exception("Sd")
+print("single_passed",single_passed)
+print("single_failed",single_failed)
+print("multiple_passed",multiple_passed)
+print('multiple_failed',multiple_failed)
+
+if False:
+	analyzer = analysis.Analyzer("AllExperimentsProtocol16", ddG_score_type = 'kellogg.total')
+	analyzer.AddPublishedDDGsToAnalysisTables()
+	reporter = analysis.Reporter(analyzer)
+	reporter.CreateReport(description = analyzer.description, outfname = 'kellogg.pdf', filetype = filetype)
+
+for score_type in ['noah_8,0A', 'noah_9,0A']:
+	for score_method in ['total', 'positional', 'positional_twoscore']:
+		analyzer = analysis.Analyzer("AllExperimentsProtocol16", ddG_score_type = '%s.%s' % (score_type, score_method))
+		analyzer.AddPublishedDDGsToAnalysisTables()
+		reporter = analysis.Reporter(analyzer)
+		reporter.CreateReport(description = analyzer.description, outfname = '%s_%s.pdf' % (score_type, score_method), filetype = filetype)
+		
 #analysis.plot(analysis._R_correlation_coefficient, analysis._createAveragedInputFile, results, "my_plot2.pdf", average_fn = analysis._mean)
 		
 		
