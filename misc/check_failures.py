3eff78a34b158ae87b09a77b0d7bc51e7c6296e7
Shane O'Connor <shane.oconnor@ucsf.edu>
2013-07-24 13:20:03 -0700

    Pushing changes to check_failures.py.

--- misc/check_failures.py
+++ misc/check_failures.py
@@ -33,7 +33,7 @@ def check_failures(prediction_set):
     results_root = '/var/cluster/temp/'
 
     UserDataSetExperimentIDs = {}
-    results = ddGdb.execute_select('''SELECT ID, ExperimentID FROM Prediction WHERE PredictionSet = 'RosettaCon2013_P16_talaris2013' AND STATUS = 'failed' ''')
+    results = ddGdb.execute_select('''SELECT ID, ExperimentID FROM Prediction WHERE PredictionSet = %s AND STATUS = 'failed' ''', parameters=(prediction_set,))
     reported_failures = [r['ID'] for r in results]
     for r in results:
         UserDataSetExperimentIDs[r['ID']] = r['ExperimentID']
@@ -201,5 +201,8 @@ def count_num_residues_in_active_jobs():
 
 
 #check_failures('RosettaCon2013_P16_talaris2013')
-fix_1TEN_InputFiles()
+
+check_failures('RosCon2013_P16_score12prime')
+#check_failures('RosCon2013_P16_talaris2013')
+#fix_1TEN_InputFiles()
 #count_num_residues_in_active_jobs()
