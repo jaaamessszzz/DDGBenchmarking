ef2c9a3a40375d1db910e68948f52f0ec7325aa0
Shane O'Connor <shane.oconnor@ucsf.edu>
2014-08-04 11:33:41 -0700

    A lot of whitespace changes and some class removals.

--- misc/check_failures.py
+++ misc/check_failures.py
@@ -119,7 +119,7 @@ def fix_1TEN_InputFiles():
     import pickle
     ddGdb = ddgdbapi.ddGDatabase()
 
-    BadPredictions = sorted(set([(r['PredictionID'], r['Status']) for r in ddGdb.execute_select("SELECT Prediction.ID AS PredictionID, Status FROM Prediction INNER JOIN Experiment ON Experiment.ID=Prediction.ExperimentID INNER JOIN ExperimentMutation ON Experiment.ID=ExperimentMutation.ExperimentID WHERE PredictionSet = 'RosCon2013_P16_talaris2013' AND PDBFileID='1TEN' ")]))
+    BadPredictions = sorted(set([(r['PredictionID'], r['Status']) for r in ddGdb.execute_select("SELECT Prediction.ID AS PredictionID, Status FROM Prediction INNER JOIN Experiment ON Experiment.ID=Prediction.ExperimentID INNER JOIN ExperimentMutation ON Experiment.ID=ExperimentMutation.ExperimentID WHERE PredictionSet = 'RosCon2013_P16_talaris2013sc' AND PDBFileID='1TEN' ")]))
     BadPredictionIDs = sorted(set([r[0] for r in BadPredictions]))
     num_active = len([r for r in BadPredictions if r[1] == 'active'])
     num_queued = len([r for r in BadPredictions if r[1] == 'queued'])
@@ -202,7 +202,7 @@ def count_num_residues_in_active_jobs():
 
 #check_failures('RosettaCon2013_P16_talaris2013')
 
-check_failures('RosCon2013_P16_score12prime')
+#check_failures('RosCon2013_P16_score12prime')
 #check_failures('RosCon2013_P16_talaris2013')
-#fix_1TEN_InputFiles()
+fix_1TEN_InputFiles()
 #count_num_residues_in_active_jobs()
