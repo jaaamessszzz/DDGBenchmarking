b28453ce81816a6d03a81a530e5fc63dadd412e2
Shane O'Connor <spiderbaby@gmail.com>
2014-12-15 18:48:24 -0800

    ...

--- misc/run_rescoring.sh
+++ misc/run_rescoring.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 rm  /tmp/klab_rescore.txt
-cd ~/RosettaCon2013_rescoring/ddg/misc/
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 1 -d &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 2  &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 3  &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 4  &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 5  &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 6  &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 7  &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 8  &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 9  &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 10 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 11 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 12 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 13 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 14 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 15 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 16 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 17 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 18 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 19 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 20 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 21 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 22 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 23 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 24 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 25 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 26 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 27 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 28 &
-nohup python rescore.py -s "Ubiquitin scan: UQ_con_yeast p16" -n 29 -p 29 &
+cd /home/oconchus/dev/ddg/misc
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 1 -d &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 2  &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 3  &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 4  &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 5  &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 6  &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 7  &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 8  &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 9  &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 10 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 11 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 12 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 13 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 14 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 15 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 16 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 17 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 18 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 19 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 20 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 21 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 22 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 23 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 24 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 25 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 26 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 27 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 28 &
+nohup python rescore.py -s "Protocol_16_r57471" -n 29 -p 29 &
 
