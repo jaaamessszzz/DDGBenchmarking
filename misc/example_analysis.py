6fff893bd509672e75c0d74b5e0f87ad1fd364d0
Shane O'Connor <spiderbaby@gmail.com>
2014-12-17 13:07:47 -0800

    Committing changes for plane work.

--- misc/example_analysis.py
+++ misc/example_analysis.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import pprint
 #sys.path.insert(0, "..")
 sys.path.insert(0, "../ddglib")
 sys.path.insert(0, "../..")
@@ -7,7 +8,8 @@ sys.path.insert(0, "..")
 
 from ddglib import analysis
 from tools import colortext
-from tools.fs.fsio import read_file, get_file_lines
+from tools.fs.fsio import read_file, get_file_lines, write_file
+import json
 
 filetype = "pdf"
 prediction_set_for_analysis = 'Protocol_16_r57471'
@@ -19,32 +21,42 @@ from ddglib import dbapi, ddgdbapi
 from tools.bio import pdb
 
 
-if __name__ == '__main__':
-    import json
-    cached_pdb_details = json.loads(read_file('cached_pdb_details.json'))
+
+def dump_data():
     ddG_connection = dbapi.ddG()
     ddGdb = ddgdbapi.ddGDatabase()
 
+    prediction_set = 'Protocol_16_r57471'
+    userdata_set = 'AllValidPGPK'
+
+    cached_pdb_details = json.loads(read_file('cached_pdb_details.json'))
+    analysis_breakdown = ddG_connection.get_predictionset_data(prediction_set, userdata_set, cached_pdb_details = cached_pdb_details)
+
+    test_data = dict(
+        amino_acids = analysis_breakdown.amino_acids,
+        pdb_details = analysis_breakdown.pdb_details,
+        predictions = analysis_breakdown.predictions,
+        analysis_datasets = analysis_breakdown.analysis_datasets,
+    )
+    write_file('example_analysis_input.json', json.dumps(test_data))
+
+
+def load_data():
+    d = json.loads(read_file('example_analysis_input.json'))
+    return dbapi.AnalysisBreakdown(d['amino_acids'], d['pdb_details'], d['predictions'], d['analysis_datasets'])
+
+
+if __name__ == '__main__':
+    import json
+
     # Retrieve the data
-    subset = 'Guerois'
-    subset = 'AlaScan-GPK'
-    data = ddG_connection.get_predictionset_data('Protocol_16_r57471', cached_pdb_details = cached_pdb_details)
-    predictions = data['predictions']
-    analysis_dataset = data['analysis_datasets'][subset]
+    analysis_breakdown = load_data()
 
-    # Analyze the data
-    from tools.stats.misc import get_xy_dataset_correlations
-    xvalues = []
-    yvalues = []
-    for prediction_id, details in sorted(predictions.iteritems()):
-        if prediction_id in analysis_dataset:
-            ExperimentalDDG = analysis_dataset[prediction_id]['ExperimentalDDG']
-            predicted_score = details['Kellogg']
-            if predicted_score != None:
-                xvalues.append(ExperimentalDDG)
-                yvalues.append(predicted_score)
-
-    colortext.message('Analyzing %d values for dataset %s.' % (len(xvalues), subset))
-    print(get_xy_dataset_correlations(xvalues, yvalues))
-
-
+    for analysis_subset in analysis_breakdown.analysis_datasets.keys():
+        if analysis_subset != 'Kellogg':
+            continue
+        for scoring_method in ['Kellogg', 'Noah']:
+            #analysis_breakdown.analyze_subset_all(analysis_subset, scoring_method)
+            analysis_breakdown.analyze_subset_by_specific_resolutions(analysis_subset, scoring_method)
+            analysis_breakdown.analyze_subset_by_binned_resolutions(analysis_subset, scoring_method)
+        break
