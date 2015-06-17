c578bbd4bd271a0433394bd70f0b0c4c68ea0fda
Shane O'Connor <spiderbaby@gmail.com>
2015-06-16 19:03:53 -0700

    Adding a user-facing API by wrapping an API object inside of another object. I added function decorators which are used to set a _helptype attribute for certain functions. I added a help function to the user-facing API which prints out the docstrings of all API functions, first grouping them by _helptype.
    This works pretty well - we have an API which hides some of the functions we do not wish to expose (you can still get to them of course) and a useful automatic help system. The help system will be useful for listing the API contents during the refactoring.

--- misc/test_job_insertion.py
+++ misc/test_job_insertion.py
@@ -1,7 +1,28 @@
+import sys
+import traceback
+import datetime
+import string
+import os
+import pickle
+import numpy
+import json
+import pprint
+
+if __name__ == "__main__":
+    sys.path.insert(0, "..")
+    sys.path.insert(0, "../..")
+    sys.path.insert(0, "../updatedb")
+
+import tools.colortext as colortext
+from tools.fs.fsio import read_file
 from ddglib.ppi_api import BindingAffinityDDGUserInterface
 
 if __name__ == '__main__':
-    ddGdb = BindingAffinityDDGUserInterface(passwd = read_file('ddgdb.pw').strip(), use_utf = False)
-    ddG_interface = ddGInterface(passwd = read_file('ddgdb.pw').strip())
+    ddg_api = BindingAffinityDDGUserInterface(passwd = read_file('ddgdb.pw').strip())
+    ddg_db = ddg_api.DDG_db
+    ddg_db_utf = ddg_api.DDG_db_utf
 
-BindingAffinityDDGUserInterface(
+
+if __name__ == '__main__':
+    print(ddg_api.help())
+
