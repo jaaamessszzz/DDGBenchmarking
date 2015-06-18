c3a553a17fc76637d59acf65ddc7197dce30c727
Shane O'Connor <spiderbaby@gmail.com>
2015-06-18 15:28:19 -0700

    I abstracted the user interface class into a generic class parameterized by the 'API' class. This gives us a much cleaner interface:
        from ddglib.ppi_api import get_interface as get_ppi_interface
        ddg_api = get_ppi_interface(read_file('ddgdb.pw'))
        ddg_api.help()
    
    or
    
        from ddglib.monomer_api import get_interface as get_protein_stability_interface
        stability_api = get_protein_stability_interface(read_file('ddgdb.pw'))
        stability_api.help()

--- misc/test_job_insertion.py
+++ misc/test_job_insertion.py
@@ -15,14 +15,19 @@ if __name__ == "__main__":
 
 import tools.colortext as colortext
 from tools.fs.fsio import read_file
-from ddglib.ppi_api import BindingAffinityDDGUserInterface
+from ddglib.ppi_api import BindingAffinityDDGInterface
+from ddglib.ppi_api import get_interface as get_ppi_interface
+from ddglib.monomer_api import MonomericStabilityDDGInterface
+from ddglib.monomer_api import get_interface as get_protein_stability_interface
 
 if __name__ == '__main__':
-    ddg_api = BindingAffinityDDGUserInterface(passwd = read_file('ddgdb.pw').strip())
+    from ddglib.ppi_api import get_interface as get_ppi_interface
+    ddg_api = get_ppi_interface(read_file('ddgdb.pw'))
+    ddg_api.help()
+
+    from ddglib.monomer_api import get_interface as get_protein_stability_interface
+    stability_api = get_protein_stability_interface(read_file('ddgdb.pw'))
+    stability_api.help()
+
     ddg_db = ddg_api.DDG_db
     ddg_db_utf = ddg_api.DDG_db_utf
-
-
-if __name__ == '__main__':
-    print(ddg_api.help())
-
