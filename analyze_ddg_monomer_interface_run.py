dd81b55a83cb3182c4e4246d9c717a62395291aa
Kyle Barlow <kb@kylebarlow.com>
2015-11-12 17:27:14 -0800

    State to rescore diubq dipubs runs

--- analyze_ddg_monomer_interface_run.py
+++ analyze_ddg_monomer_interface_run.py
@@ -14,10 +14,7 @@ def process_ddg_monomer_directory( job_dir ):
     settings = parse_settings.get_dict()
     rosetta_scripts_path = settings['local_rosetta_installation_path'] + '/source/bin/' + 'rosetta_scripts' + settings['local_rosetta_binary_type']
 
-    date, prediction_set_name = os.path.basename(job_dir).split('-')
-    # Strip user name off prediction_set_name
-    if getpass.getuser() in prediction_set_name:
-        prediction_set_name = prediction_set_name.strip( getpass.getuser() + '_' )
+    prediction_set_name = 'DiPUBS: Complexes #1'
     
     ppi_api = get_interface_with_config_file(rosetta_scripts_path = rosetta_scripts_path, rosetta_database_path = '/home/kyleb/rosetta/working_branches/alascan/database', get_interface_factory = get_interface_factory )
     score_method_id = 7 # rescore with interface weigths
