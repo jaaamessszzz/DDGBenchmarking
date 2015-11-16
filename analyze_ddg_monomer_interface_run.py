e8860c83f8704af97e727920f9420c6ffa23b9bd
Kyle Barlow <kb@kylebarlow.com>
2015-11-16 09:14:18 -0800

    Fixed horrible yeast bug and finally figured out how to make folders not too big

--- analyze_ddg_monomer_interface_run.py
+++ analyze_ddg_monomer_interface_run.py
@@ -19,11 +19,11 @@ def process_ddg_monomer_directory( job_dir ):
     ppi_api = get_interface_with_config_file(rosetta_scripts_path = rosetta_scripts_path, rosetta_database_path = '/home/kyleb/rosetta/working_branches/alascan/database', get_interface_factory = get_interface_factory )
     score_method_id = 7 # rescore with interface weigths
     # To run live:
-    # ppi_api.extract_data(prediction_set_name, root_directory = job_dir, score_method_id = score_method_id, max_prediction_ids_to_process = 40)
+    # ppi_api.extract_data(prediction_set_name, root_directory = job_dir, score_method_id = score_method_id)#, max_prediction_ids_to_process = 40)
     # To setup cluster run:
-    # ppi_api.extract_data(prediction_set_name, root_directory = job_dir, score_method_id = score_method_id, setup_cluster_run = True)
+    ppi_api.extract_data(prediction_set_name, root_directory = job_dir, score_method_id = score_method_id, setup_cluster_run = True)
     # To process cluster run:
-    process_cluster_run(ppi_api, prediction_set_name, score_method_id, settings)
+    # process_cluster_run(ppi_api, prediction_set_name, score_method_id, settings)
 
 def process_cluster_run(ppi_api, prediction_set_name, score_method_id, settings):
     output_dir = '/dbscratch/kyleb/tmp/cluster_run/DiPUBSComplexes1'
