8e95903a9c74787c06c0c079ac7ae1559b3c5190
Shane O'Connor <spiderbaby@gmail.com>
2015-12-11 15:40:53 -0800

    Updating the example code.

--- misc/pooja.py
+++ misc/pooja.py
@@ -1,13 +1,16 @@
 import sys
 import pprint
+import glob
 
-if False:
+if True:
   if __name__ == "__main__":
     sys.path.insert(0, "..")
     sys.path.insert(0, "../..")
     sys.path.insert(0, "../updatedb")
     sys.path.insert(0, '/home/oconchus/dev/')
     sys.path.insert(0, "/home/oconchus/dev/klab")
+else:
+    import klab
 
 import klab.colortext as colortext
 from ddglib.ppi_api import get_interface_with_config_file as get_ppi_interface_with_config_file
@@ -26,6 +29,16 @@ colortext.message('Connected to database.')
 score_method_id = ppi_api.get_score_method_id('Rescore-Talaris2014', method_authors = 'kyle', method_type = 'ddg_monomer rescore')
 
 # Get the best structures for prediction 23849
-pprint.pprint(ppi_api.get_top_x_scores(23849, score_method_id, 'WildTypeComplex', 3, component = 'total', order_by = 'ASC'))
-pprint.pprint(ppi_api.get_top_x_scores(23849, score_method_id, 'MutantComplex', 3, component = 'total', order_by = 'ASC'))
+wild_type_complexes = ppi_api.get_top_x_scores(23849, score_method_id, 'WildTypeComplex', 3, component = 'total', order_by = 'ASC')
+wild_type_filenames = []
+for wtc in wild_type_complexes:
+    wild_type_filenames.append([f for f in glob.glob('repacked_wt*_round_{0}.*'.format(wtc['StructureID']))][0])
+print(wild_type_filenames)
+
+
+mutant_complexes = ppi_api.get_top_x_scores(23849, score_method_id, 'MutantComplex', 3, component = 'total', order_by = 'ASC')
+mutant_filenames = []
+for mtc in mutant_complexes:
+    mutant_filenames.append([f for f in glob.glob('mut*_round_{0}.*'.format(wtc['StructureID']))][0])
+print(mutant_filenames)
 
