db61059aa7483574cb4ed739b7d05c0ed9f22e9e
Kyle Barlow <kb@kylebarlow.com>
2016-01-12 20:56:55 -0800

    Adding configuration file for zemu ddg_monomer rerun

---
+++ run_config/zemu2.py
@@ -0,0 +1,6 @@
+prediction_set_id = 'ddg_monomer_16_003-zemu-2' # This should be a valid filename
+protocol_name = 'ddg_monomer_16_003'
+
+user_dataset_name = 'AllBindingAffinity'
+tagged_subset = 'ZEMu'
+prediction_set_description = 'New run of ddg_monomer_16 on ZEMU, hopefully with occupancy/residue loading issues resolved'
