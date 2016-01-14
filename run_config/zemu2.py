db83d9eb6f8e0bd37869cd8aac3f2397fbbfbf8c
Kyle Barlow <kb@kylebarlow.com>
2016-01-13 16:39:01 -0800

    Adding new setting required by analysis script

--- run_config/zemu2.py
+++ run_config/zemu2.py
@@ -4,3 +4,5 @@ protocol_name = 'ddg_monomer_16_003'
 user_dataset_name = 'AllBindingAffinity'
 tagged_subset = 'ZEMu'
 prediction_set_description = 'New run of ddg_monomer_16 on ZEMU, hopefully with occupancy/residue loading issues resolved'
+
+score_method_id = 7 # rescore with interface weights score method (this is probably best...need to benchmark more)
