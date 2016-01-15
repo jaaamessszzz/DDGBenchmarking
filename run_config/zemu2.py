557a8e77a215ca518e540d204cbcffd08705a026
Kyle Barlow <kb@kylebarlow.com>
2016-01-14 18:38:39 -0800

    End analysis script now using config file

--- run_config/zemu2.py
+++ run_config/zemu2.py
@@ -6,3 +6,6 @@ tagged_subset = 'ZEMu'
 prediction_set_description = 'New run of ddg_monomer_16 on ZEMU, hopefully with occupancy/residue loading issues resolved'
 
 score_method_id = 7 # rescore with interface weights score method (this is probably best...need to benchmark more)
+
+# Prediction set analysis
+prediction_set_credit = 'Kyle Barlow'
