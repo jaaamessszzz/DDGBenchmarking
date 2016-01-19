a6edb3848d32550f307c139728ab6edb20e0725a
Kyle Barlow <kb@kylebarlow.com>
2016-01-19 14:08:08 -0800

    Add config file for run with beta_nov15 score function

---
+++ run_config/zemu2_betanov15.py
@@ -0,0 +1,13 @@
+prediction_set_id = 'ddg_monomer_16-zemu-betanov15' # This should be a valid filename
+protocol_name = 'ddg_monomer_16-betanov15'
+
+user_dataset_name = 'AllBindingAffinity'
+tagged_subset = 'ZEMu'
+prediction_set_description = 'ddg_monomer_16 on ZEMU with beta Nov 2015 score function'
+extra_flags = ['-beta_nov15']
+
+score_method_id = 7 # rescore with interface weights score method (this is probably best...need to benchmark more)
+# score_method_id = 8 # rescore with talaris weights score method
+
+# Prediction set analysis
+prediction_set_credit = 'Kyle Barlow'
