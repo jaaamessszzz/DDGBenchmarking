b0e5df0eb7e7de15968d23c7bec277caa020a540
James Lucas <james.lucas@berkeley.edu>
2016-03-21 20:41:09 -0700

    Created .py function to parse through Rosetta.out files and store DDG scores in nested dictionaries

---
+++ RosettaOut_Parser.py
@@ -0,0 +1,55 @@
+#Iterates through directories to open Rosetta.out and creates Nested Dictionaries:
+#>Prediction ID
+#>>Structure ID (nstruct 1-100)
+#>>>Structure Type (WT, Mut, DDG)
+#>>>>Score Type
+
+import os
+import linecache
+
+def parse_rosetta_out(workingdir):
+    fattydict = {}
+    for i in os.listdir(workingdir):
+        if os.path.isdir(workingdir + i):
+            fattydict[i] = {}
+            structID = 1
+            counter = 0
+            filename = workingdir + i + "/rosetta.out"
+            for line in enumerate(open(filename, 'r')):
+                if line[1].find("fa_atr") == 1:
+                    if counter % 2 == 0:
+                        linecounter = 0
+                        currentline = line[0] + 1
+                        fattydict[i]['WT_' + str(structID)] = {}
+                        while linecounter < 15:
+                            scores = linecache.getline(filename, currentline)
+                            parsed_scores = scores.split()
+                            fattydict[i]['WT_' + str(structID)][parsed_scores[0]] = parsed_scores[1]
+                            linecounter = linecounter + 1
+                            currentline = currentline + 1
+                        sumscore = linecache.getline(filename, line[0] + 17)
+                        parsed_sumscore = sumscore.split()
+                        fattydict[i]['WT_' + str(structID)][parsed_sumscore[0]] = parsed_sumscore[2]
+                        counter = counter + 1
+                    else:
+                        linecounter = 0
+                        currentline = line[0] + 1
+                        fattydict[i]['Mutant_' + str(structID)] = {}
+                        while linecounter < 15:
+                            scores = linecache.getline(filename, currentline)
+                            parsed_scores = scores.split()
+                            fattydict[i]['Mutant_' + str(structID)][parsed_scores[0]] = parsed_scores[1]
+                            linecounter = linecounter + 1
+                            currentline = currentline + 1
+                        sumscore = linecache.getline(filename, line[0] + 17)
+                        parsed_sumscore = sumscore.split()
+                        fattydict[i]['Mutant_' + str(structID)][parsed_sumscore[0]] = parsed_sumscore[2]
+                        counter = counter + 1
+                        structID = structID +1
+                else:
+                    continue
+    return fattydict
+
+my_working_directory = '/Users/jameslucas/Kortemme_Rotation/output/'
+parsed_dict = parse_rosetta_out(my_working_directory)
+print parsed_dict
