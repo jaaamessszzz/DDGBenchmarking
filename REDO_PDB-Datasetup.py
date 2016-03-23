1d6b6fd5ebf6c352a8fba82aec301b23cc451045
James Lucas <james.lucas@berkeley.edu>
2016-03-22 18:41:27 -0700

    fixed shutil.copy2

--- REDO_PDB-Datasetup.py
+++ REDO_PDB-Datasetup.py
@@ -15,4 +15,4 @@ for i in jsoninfo:
     #Copy Resfiles and stuff from source to PDB_REDO data directory
     shutil.copytree(source + i, dest + i, ignore = shutil.ignore_patterns('*.pdb'))
     #Copy PDB_REDO pdb's from PDB_REDO directory to new data directory
-    shutil.copy2(PDB_REDO + i + "/" + pdbfile, dest + i + "/" + pdbfile)
+    shutil.copy2(PDB_REDO + "/" + pdbfile, dest + i + "/" + pdbfile)
