985826059bb078cb391c8ac0cd5671794fd574a6
James Lucas <james.lucas@berkeley.edu>
2016-03-29 14:54:35 -0700

    changed freemem

--- RunScripts/SubmitRun_DDG_Zemu_General.py
+++ RunScripts/SubmitRun_DDG_Zemu_General.py
@@ -5,7 +5,7 @@
 #$ -l h_rt=240:00:00
 #$ -t 1-1240
 #$ -l arch=linux-x64
-#$ -l mem_free=4.5G
+#$ -l mem_free=1.9G
 #$ -l netapp=2G,scratch=1G
 
 # Make sure you set task number above to be correct!!!
