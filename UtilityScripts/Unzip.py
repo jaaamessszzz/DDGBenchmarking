d78a78fe59649c3befb6db27654a879b8faf1c54
jaaamessszzz <james.lucas@berkeley.edu>
2016-04-06 16:07:50 -0700

    updated version of Unzip.py

--- UtilityScripts/Unzip.py
+++ UtilityScripts/Unzip.py
@@ -1,10 +1,14 @@
 import os
 import zipfile
 
-def unzip():
-    for file listdir(donefolder):
-        print file[:4]
-        os.mkdir(file[:4])
-        zipfile.extractall(file, path = file[:4]
-
-donefolder = os.
+def unzip(donefolder):
+    for file in os.listdir(donefolder):
+        print file[:-4]
+        os.mkdir(file[:-4])
+        targetzip = zipfile.ZipFile(file, mode = 'r')
+        targetzip.extractall(path = file[:-4])
+                                   
+os.chdir('/home/james.lucas/Rotation/done')                                   
+#os.chdir('/netapp/home/james.lucas/160322-james-backrub-rscript-full/output/done')
+asdf = os.getcwd()
+unzip(asdf)
