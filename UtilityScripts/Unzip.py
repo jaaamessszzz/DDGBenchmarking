7483dd43aaf98fb74ea7d81c9eed1aba3f331259
jaaamessszzz <james.lucas@berkeley.edu>
2016-04-06 15:51:13 -0700

    Unzipping script to fix compression issue with Zippify.py

---
+++ UtilityScripts/Unzip.py
@@ -0,0 +1,10 @@
+import os
+import zipfile
+
+def unzip():
+    for file listdir(donefolder):
+        print file[:4]
+        os.mkdir(file[:4])
+        zipfile.extractall(file, path = file[:4]
+
+donefolder = os.
