02c3598dfc3f0cdf87a87771b33847a5dc1b901b
jaaamessszzz <james.lucas@berkeley.edu>
2016-04-08 18:55:56 -0700

    sync

--- UtilityScripts/Zippify.py
+++ UtilityScripts/Zippify.py
@@ -1,17 +1,18 @@
 import os
 import shutil
 import zipfile
+import zlib
 
 def zippify(outputdir):
     for dirname, subdirs, files in os.walk(outputdir):
         if len(files) == 104: ##Number of desired files present
             os.chdir(dirname)
-            myzip = zipfile.ZipFile('%s.zip' % dirname, mode = 'w')
+            myzip = zipfile.ZipFile('%s.zip' % dirname, mode = 'w', compression = ZIP_DEFLATED)
             for j in files:
                 myzip.write(j)
             myzip.close()
             shutil.rmtree(dirname)
-            home
+            os.chdir('/netapp/home/james.lucas/160322-james-backrub-rscript-full/output/')
         else:
             continue
             
