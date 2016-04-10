b048d7ac69e614e9dc1a94e6477235b79a8707ab
James Lucas <james.lucas@berkeley.edu>
2016-04-10 14:52:05 -0700

    asdf

--- UtilityScripts/Zippify.py
+++ UtilityScripts/Zippify.py
@@ -1,18 +1,17 @@
 import os
 import shutil
 import zipfile
-import zlib
 
 def zippify(outputdir):
     for dirname, subdirs, files in os.walk(outputdir):
         if len(files) == 104: ##Number of desired files present
             os.chdir(dirname)
-            myzip = zipfile.ZipFile('%s.zip' % dirname, mode = 'w', compression = ZIP_DEFLATED)
+            myzip = zipfile.ZipFile('%s.zip' % dirname, mode = 'w')
             for j in files:
                 myzip.write(j)
             myzip.close()
             shutil.rmtree(dirname)
-            os.chdir('/netapp/home/james.lucas/160322-james-backrub-rscript-full/output/')
+            home
         else:
             continue
             
