dea69e0fb089e5f065d5f7d02a0cf60c9fd75176
James Lucas <james.lucas@berkeley.edu>
2016-05-11 15:11:09 -0700

    Made improvements to List_PDB.py (uses sets now to store unique PDB IDs)

--- UtilityScripts/List_PDBs.py
+++ UtilityScripts/List_PDBs.py
@@ -1,13 +1,22 @@
+#Prints out all PDB files in subdirectories of workingdir
+#List_PDBs.py
 import os
+import csv
 
-workingdir = '/netapp/home/james.lucas/160322-james-backrub-rscript-full/data/'
+workingdir = '/Users/jameslucas/Kortemme_Rotation/PDB_REDO'
+#workingdir = '/netapp/home/james.lucas/160315-kyleb_james-backrub-rscript/data/'
 #workingdir = '/Users/jameslucas/Kortemme_Rotation/output/'
+
+pdbcsv = open('PDB_list.csv', 'wb')
+csvwriter = csv.writer(pdbcsv, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+pdb_set = set()
 for i in os.listdir(workingdir):
-    if os.path.isdir(workingdir+i):
-        for j in os.listdir(workingdir+i):
-            if j.endswith('.pdb'):
-                print j
-            else:
-                continue
+    if i.endswith('.pdb'):
+        pdb_set.add(i)
     else:
         continue
+
+for pdbid in pdb_set:
+    print pdbid
+    csvwriter.writerow([pdbid])
